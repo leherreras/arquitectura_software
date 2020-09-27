20e383a85237b1650e0a7d6dc535b067a38d938f
Luis Carlos Herrera <luis.herrera@appgate.com>
2020-09-27 11:26:12 -0500

    delete file install db and create all db in views.py

--- back/src/entities/users.py
+++ back/src/entities/users.py
@@ -1,4 +1,3 @@
-from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from src.views import db
--- back/src/views.py
+++ back/src/views.py
@@ -7,6 +7,11 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = codecs.encode(os.urandom(32), 'hex')
 db_name = 'architecture.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./{}'.format(db_name)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../{}'.format(db_name)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+ROOT_PATH = os.path.abspath(os.path.join(__file__, '..', '..'))
+if not os.path.isfile(os.path.join(ROOT_PATH, db_name)):
+    db.create_all()
